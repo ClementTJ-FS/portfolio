a2a2ca3df2dfb9234a2395dc01473d0e8ad88042
ClementTJ-FS <TMendoza1@student.fullsail.edu>
2022-09-09 19:29:40 -0700

    Added header/nav. Added Initial Styling.

--- dev/portfolio/src/App.js
+++ dev/portfolio/src/App.js
@@ -1,5 +1,90 @@
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+// import About from './components/About';
+// import Projects from './components/Projects';
+// import Contact from './components/Contact';
+
 function App() {
-  return <div className='App'></div>;
+  const styles = {
+    test: {
+      marginTop: '10rem',
+      fontSize: '2rem',
+    },
+    h1: {
+      textAlign: 'center',
+      fontSize: '3rem',
+      marginBottom: '2rem',
+    },
+  };
+
+  const [scroll, setScroll] = useState(0);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    //check if mobile
+    window.innerWidth <= 768 ? setIsMobile(true) : setIsMobile(false);
+    //check scroll
+    if (!isMobile) {
+      window.addEventListener('scroll', () => {
+        setScroll(window.scrollY);
+      });
+    } else if (isMobile) {
+      setScroll(0);
+    }
+  }, [isMobile]);
+
+  return (
+    <div className='App'>
+      {!isMobile && <h1 style={styles.h1}>TJ Clement</h1>}
+      <Header scroll={scroll} isMobile={isMobile} />
+      <div>
+        <p style={styles.test}>
+          sada sdasd sad asd asd asdasdadadasd asdad asda dada dasada sdasd sad
+          asd asd asdasdadadasd asdad asda dada dasada sdasd sad asd asd
+          asdasdadadasd asdad asda dada dasada sdasd sad asd asd asdasdadadasd
+          asdad asda dada dasada sdasd sad asd asd asdasdadadasd asdad asda dada
+          dasada sdasd sad asd asd asdasdadadasd asdad asda dada dasada sdasd
+          sad asd asd asdasdadadasd asdad asda dada dasada sdasd sad asd asd
+          asdasdadadasd asdad asda dada dasada sdasd sad asd asd asdasdadadasd
+          asdad asda dada dasada sdasd sad asd asd asdasdadadasd asdad asda dada
+          dasada sdasd sad asd asd asdasdadadasd asdad asda dada dasada sdasd
+          sad asd asd asdasdadadasd asdad asda dada dasada sdasd sad asd asd
+          asdasdadadasd asdad asda dada da sada sdasd sad asd asd asdasdadadasd
+          asdad asda dada dasada sdasd sad asd asd asdasdadadasd asdad asda dada
+          dasada sdasd sad asd asd asdasdadadasd asdad asda dada dasada sdasd
+          sad asd asd asdasdadadasd asdad asda dada dasada sdasd sad asd asd
+          asdasdadadasd asdad asda dada dasada sdasd sad asd asd asdasdadadasd
+          asdad asda dada dasada sdasd sad asd asd asdasdadadasd asdad asda dada
+          dasada sdasd sad asd asd asdasdadadasd asdad asda dada dasada sdasd
+          sad asd asd asdasdadadasd asdad asda dada dasada sdasd sad asd asd
+          asdasdadadasd asdad asda dada dasada sdasd sad asd asd asdasdadadasd
+          asdad asda dada dasada sdasd sad asd asd asdasdadadasd asdad asda dada
+          dasada sdasd sad asd asd asdasdadadasd asdad asda dada da sada sdasd
+          sad asd asd asdasdadadasd asdad asda dada dasada sdasd sad asd asd
+          asdasdadadasd asdad asda dada dasada sdasd sad asd asd asdasdadadasd
+          asdad asda dada dasada sdasd sad asd asd asdasdadadasd asdad asda dada
+          dasada sdasd sad asd asd asdasdadadasd asdad asda dada dasada sdasd
+          sad asd asd asdasdadadasd asdad asda dada dasada sdasd sad asd asd
+          asdasdadadasd asdad asda dada dasada sdasd sad asd asd asdasdadadasd
+          asdad asda dada dasada sdasd sad asd asd asdasdadadasd asdad asda dada
+          dasada sdasd sad asd asd asdasdadadasd asdad asda dada dasada sdasd
+          sad asd asd asdasdadadasd asdad asda dada dasada sdasd sad asd asd
+          asdasdadadasd asdad asda dada dasada sdasd sad asd asd asdasdadadasd
+          asdad asda dada da sada sdasd sad asd asd asdasdadadasd asdad asda
+          dada dasada sdasd sad asd asd asdasdadadasd asdad asda dada dasada
+          sdasd sad asd asd asdasdadadasd asdad asda dada dasada sdasd sad asd
+          asd asdasdadadasd asdad asda dada dasada sdasd sad asd asd
+          asdasdadadasd asdad asda dada dasada sdasd sad asd asd asdasdadadasd
+          asdad asda dada dasada sdasd sad asd asd asdasdadadasd asdad asda dada
+          dasada sdasd sad asd asd asdasdadadasd asdad asda dada dasada sdasd
+          sad asd asd asdasdadadasd asdad asda dada dasada sdasd sad asd asd
+          asdasdadadasd asdad asda dada dasada sdasd sad asd asd asdasdadadasd
+          asdad asda dada dasada sdasd sad asd asd asdasdadadasd asdad asda dada
+          dasada sdasd sad asd asd asdasdadadasd asdad asda dada da
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ dev/portfolio/src/components/Header.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import './Header.css';
+
+const Header = ({ scroll, isMobile }) => {
+  //scroll effect
+  useEffect(() => {
+    if (scroll > 50 || isMobile) {
+      document.querySelector('.navbar').classList.remove('mt-5');
+      //transition effect
+      document.querySelector('.navbar').classList.add('navbar-transition');
+      document.querySelector('.navbar').classList.remove('bg-transparent');
+      document.querySelector('.navbar').classList.remove('navbar-light');
+      document.querySelector('.navbar').classList.add('bg-dark');
+      document.querySelector('.navbar').classList.add('navbar-dark');
+    } else if (scroll < 50) {
+      document.querySelector('.navbar').classList.add('mt-5');
+      document.querySelector('.navbar').classList.add('bg-transparent');
+      document.querySelector('.navbar').classList.remove('bg-dark');
+      document.querySelector('.navbar').classList.remove('navbar-dark');
+      document.querySelector('.navbar').classList.add('navbar-light');
+    }
+  }, [scroll, isMobile]);
+
+  // check if mobile
+  useEffect(() => {
+    if (isMobile) {
+      document.querySelector('.navbar').classList.add('justify-content-end');
+      document.querySelector('.navbar').classList.remove('navbar-transition');
+    }
+  }, [isMobile]);
+
+  return (
+    <header>
+      <Container>
+        <Navbar
+          id='navbar'
+          collapseOnSelect
+          expand='md'
+          bg='transparent'
+          variant='light'
+          className='px-3 mt-5 shadow-sm'
+          fixed='top'
+        >
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse
+            id='responsive-navbar-nav'
+            className='justify-content-center'
+          >
+            <Nav>
+              <Nav.Link href='#about'>About Me</Nav.Link>
+              <Nav.Link href='#projects'>Projects</Nav.Link>
+              <Nav.Link href='#contact'>Contact</Nav.Link>
+              <Nav.Link href='resume.pdf'>Resume</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
