db4cd33529a5e2b30b0d46cf6dd7a46b9dbcb39c
ClementTJ-FS <TMendoza1@student.fullsail.edu>
2022-09-14 12:35:02 -0700

    Started about component.

---
+++ dev/portfolio/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return <section style={styles.container}>About</section>;
+};
+
+const styles = {
+  container: {
+    height: '90rem',
+    backgroundColor: 'black',
+    color: 'white',
+  },
+};
+
+export default About;
