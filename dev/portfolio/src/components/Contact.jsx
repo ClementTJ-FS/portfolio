4f6c5f5ee5e1bdf6ac48fee70a47df23f20eed9c
ClementTJ-FS <TMendoza1@student.fullsail.edu>
2022-09-24 11:49:14 -0700

    Added emailjs to contact form. - Added status messages.

--- dev/portfolio/src/components/Contact.jsx
+++ dev/portfolio/src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 import Styled from 'styled-components';
+import { send } from '@emailjs/browser';
 
 const ContactContainer = Styled.section`
   display: flex;
@@ -11,6 +12,18 @@ const ContactContainer = Styled.section`
   padding-bottom: 5rem;
   width: 100%;
   box-shadow: 0px 0px 15px 5px rgba(0,0,0,0.75);
+
+  .err {
+    color: red;
+    font-size: 1.5rem;
+    text-align: center;
+  }
+
+  .succ {
+    color: green;
+    font-size: 1.5rem;
+    text-align: center;
+  }
 `;
 
 const CForm = Styled(Form)`
@@ -22,21 +35,76 @@ const CForm = Styled(Form)`
 `;
 
 const Contact = () => {
+  const [data, setData] = React.useState({
+    cName: '',
+    cEmail: '',
+    cMessage: '',
+  });
+  const [errorMsg, setErrorMsg] = React.useState(null);
+  const [successMsg, setSuccessMsg] = React.useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    send('service_4043d22', 'template_jrzrjdh', data, 'CL0Ob1LHZoc2hCns7')
+      .then((response) => {
+        setSuccessMsg('Message sent successfully!');
+        setData({
+          cName: '',
+          cEmail: '',
+          cMessage: '',
+        });
+      })
+      .catch((err) => {
+        setErrorMsg('Something went wrong, please try again.');
+      });
+  };
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <ContactContainer id='contact'>
       <h1 style={styles.h1}>Contact</h1>
-      <CForm>
+      {errorMsg && <p className='err'>{errorMsg}</p>}
+      {successMsg && <p className='succ'>{successMsg}</p>}
+      <CForm onSubmit={handleSubmit}>
         <Form.Group className='mb-3' controlId='formName'>
           <Form.Label>Name</Form.Label>
-          <Form.Control type='text' placeholder='Name' />
+          <Form.Control
+            type='text'
+            placeholder='Name'
+            name='cName'
+            onChange={handleChange}
+            value={data.cName}
+            required
+          />
         </Form.Group>
         <Form.Group className='mb-3' controlId='formEmail'>
           <Form.Label>E-Mail Address</Form.Label>
-          <Form.Control type='email' placeholder='E-Mail Address' />
+          <Form.Control
+            type='email'
+            placeholder='E-Mail Address'
+            name='cEmail'
+            onChange={handleChange}
+            value={data.cEmail}
+            required
+          />
         </Form.Group>
         <Form.Group className='mb-3' controlId='formMessage'>
           <Form.Label>Message</Form.Label>
-          <Form.Control as='textarea' rows={3} placeholder='Message' />
+          <Form.Control
+            as='textarea'
+            rows={3}
+            placeholder='Message'
+            name='cMessage'
+            onChange={handleChange}
+            value={data.cMessage}
+            required
+          />
         </Form.Group>
         <Button style={styles.btn} type='submit'>
           Submit
